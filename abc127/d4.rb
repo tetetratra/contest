ca50eacaa24fdd42db7c20071162768cdcbcefc0
tetetratra <44367208+tetetratra@users.noreply.github.com>
2019-05-26 00:23:46 +0900

    できず。。。

--- abc127/d4.rb
+++ abc127/d4.rb
@@ -1,13 +1,16 @@
+require 'irb'
 N, M = gets.split.map &:to_i
-aaa = gets.split.map(&:to_i).sort
+aaa = gets.split.map(&:to_i)
+bbb = []
+
+binding.irb
 while g = gets&.split&.map(&:to_i)
-  g[0].times do
-    aaa << g[1]
-  end
+  bbb << [g[0]] * g[1]
+  break if bbb.size >= N
 end
-aaa.sort!
+bbb.sort_by { |b| b[1] }
 sum = 0
 N.times do |i|
-  sum = aaa[N - i]
+  sum += aaa[aaa.size - i - 1]
 end
 puts sum

02983c784b3043bb0a72c07e66c1a88c353cc03d
tetetratra <44367208+tetetratra@users.noreply.github.com>
2019-06-09 18:55:24 +0900

    つくってみた

---
+++ array_split.rb
@@ -0,0 +1,18 @@
+class Array
+  def split(&ite)
+    ite ||= proc { |a| !a }
+    result = []
+    tmp = []
+    each do |a|
+      if ite.call(a)
+        result << tmp
+        tmp = []
+      else
+        tmp << a
+      end
+    end
+    result << tmp
+    result.delete_if(&:empty?)
+    result
+  end
+end

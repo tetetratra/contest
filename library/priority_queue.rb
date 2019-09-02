b0d56b70de8b84911764a5a8d04941bcf2f007b8
tetetratra <44367208+tetetratra@users.noreply.github.com>
2019-09-02 11:57:22 +0900

    procうけとりOK

--- library/priority_queue.rb
+++ library/priority_queue.rb
@@ -1,7 +1,7 @@
 require 'pp'
-
 class PriorityQueue
-  def initialize(array = [])
+  def initialize(array = [], &cmp)
+    @cmp = cmp || Proc.new { |small, big| small < big }
     @heap = []
     array.each { |a| push(a) }
   end
@@ -10,10 +10,18 @@ class PriorityQueue
     @heap.size
   end
 
+  def empty?
+    @heap.size == 0
+  end
+
+  def inspect # デバック用
+    @heap.sort.join(' < ')
+  end
+
   def push(x)
     @heap << x
     index = size - 1
-    while(@heap[pi(index)] < @heap[index])
+    while(@cmp.call(@heap[pi(index)],@heap[index]))
       @heap[pi(index)], @heap[index] = @heap[index], @heap[pi(index)]
       index = pi(index)
       break if index == 0
@@ -25,18 +33,18 @@ class PriorityQueue
     case size
     when 0 then return nil
     when 1 then return @heap.delete_at(0)
-    when 2 then return @heap[0] > @heap[1] ? @heap.delete_at(0) : @heap.delete_at(1)
+    when 2 then return @cmp.call(@heap[1], @heap[0]) ? @heap.delete_at(0) : @heap.delete_at(1)
     end
     popped = @heap[0]
     @heap[0] = @heap.delete_at(-1)
     index = 0
     loop do
       break if lci(index) >= size || rci(index) >= size
-      if @heap[lci(index)] > @heap[rci(index)] && @heap[lci(index)] > @heap[index]
-        @heap[lci(index)], @heap[index] = @heap[index], @heap[lci(index)]
+      if @cmp.call(@heap[rci(index)], @heap[lci(index)]) && @cmp.call(@heap[index], @heap[lci(index)])
+        swap!(lci(index), index)
         index = lci(index)
-      elsif @heap[rci(index)] >= @heap[lci(index)] && @heap[rci(index)] > @heap[index]
-        @heap[rci(index)], @heap[index] = @heap[index], @heap[rci(index)]
+      elsif !(@cmp.call(@heap[rci(index)], @heap[lci(index)])) && @cmp.call(@heap[index], @heap[rci(index)])
+        swap!(rci(index), index)
         index = rci(index)
       else
         break
@@ -48,6 +56,11 @@ class PriorityQueue
   alias :<< :push
 
   private
+
+  def swap!(a, b)
+    @heap[a], @heap[b] = @heap[b], @heap[a]
+    nil
+  end
 
   def parent_index(i)
     i.odd? ? ((i+1)/2)-1 : (i/2)-1
@@ -64,40 +77,4 @@ class PriorityQueue
   alias :pi :parent_index
   alias :lci :left_child_index
   alias :rci :right_child_index
-end
-
-class PriorityQueueWithoutHeap
-  def initialize(array = [])
-    @data = []
-    array.each{|a| push(a)}
-  end
-
-  def push(element)
-    @data.push(element)
-  end
-
-  def pop
-    @data.sort!.shift
-  end
-end
-
-require 'benchmark'
-@test_data = 100_000.times.map{rand(100_000)}
-@test_cnt = 1_000
-Benchmark.bm 10 do |r|
-  r.report 'PQWH' do
-    pq = PriorityQueueWithoutHeap.new(@test_data)
-    @test_cnt.times do
-      pq.push(rand(100_000))
-      pq.pop
-    end
-  end
-
-  r.report 'PQ' do
-    pq = PriorityQueue.new(@test_data)
-    @test_cnt.times do
-      pq.push(rand(100_000))
-      pq.pop
-    end
-  end
 end

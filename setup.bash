ebafa39be9d8a15b70e29de48b9b3ef9791b89d4
tetetratra <44367208+tetetratra@users.noreply.github.com>
2019-09-06 23:39:05 +0900

    ⭐D  library/library.rbA  library/main.rbM  setup.bash

--- setup.bash
+++ setup.bash
@@ -1,4 +1,4 @@
-cd ~/programing/contest/atcoder
+cd ~/contest
 mkdir $1 || exit
 cd $1
 
@@ -8,26 +8,10 @@ mkdir c
 mkdir d
 
 touch a/a.rb
-touch a/1
-touch a/2
-touch a/3
-
 touch b/b.rb
-touch b/1
-touch b/2
-touch b/3
-
 touch c/c.rb
-touch c/1
-touch c/2
-touch c/3
-
 touch d/d.rb
-touch d/1
-touch d/2
-touch d/3
 
-
-cat ~/programing/contest/library.rb | tee b/b.rb | tee a/a.rb | tee c/c.rb > d/d.rb
+cat ~/contest/library/main.rb | tee b/b.rb | tee a/a.rb | tee c/c.rb > d/d.rb
 
 echo "SET UP OK!!"

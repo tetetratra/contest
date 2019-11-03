94b67244c82a2e9d2ac13b952d99c3e3ec72dfb1
tetetratra <44367208+tetetratra@users.noreply.github.com>
2019-11-03 14:44:52 +0900

    ***

--- future_contest_2020_qual/a.rb
+++ future_contest_2020_qual/a.rb
@@ -1,5 +1,3 @@
-require "pry"
-require 'pp'
 n,M,B = gets.split.map(&:to_i)
 gy,gx = gets.split.map(&:to_i)
 c = Array.new(n){Array.new(n, ' ')}
@@ -10,8 +8,10 @@ M.times do # [向き]
   c[ry.to_i][rx.to_i] = ' '
 end
 
+b = []
 B.times do # ブロックます
   by,bx = gets.split.map(&:to_i)
+  b << [bx,by]
   c[by][bx] = 'x'
 end
 
@@ -22,28 +22,54 @@ c.map!{|cc|
   cc[gx..(-1)] + cc[0..(gx-1)]
 }
 
-5.times do
+cnt1 = 0; cnt2 = 10
+
+cnt1.times do
+  n.times do |y|
+    n.times do |x|
+      if [
+        c[y][x],
+        c[y+1] ? c[y+1][x] : c[0][x],
+        c[y][x+1],
+        c[y+1] ? c[y+1][x+1] : c[0][x+1]
+      ].compact.count{|ccc|ccc=='x'} > 1
+        c[y][x] = '*'
+        if c[y+1]
+          c[y+1][x] = '*'
+          c[y+1][x+1] = '*'
+        else
+          c[0][x] = '*'
+          c[0][x+1] = '*'
+        end
+        c[y][x+1]   = '*'
+      end
+    end
+  end
+  c.map!{|cc|cc.map!{|ccc|ccc == '*' ? 'x' : ccc}}
+end
+
+cnt2.times do
   n.times do |y|
     n.times do |x|
       next if c[y][x] == 'G'
       if (0 <= y && y < n/2)
         if (0 <= x && x < n/2) # 左上
-          if (y > x) # 左上の|\
+          if (y >= x) # 左上の|\
             if c[y-1][x] == 'x' && c[y][x-1] == 'x'
               c[y][x] = '*' unless c[y][x] == 'x'
             end
-          elsif(x > y) # 左上の\|
+          elsif(x >= y) # 左上の\|
             if c[y-1][x] == 'x' && c[y][x-1] == 'x'
               c[y][x] = '*' unless c[y][x] == 'x'
             end
           else # 左上の\の線
           end
         else # 右上
-          if (y < n-x) # 右上の/|
+          if (y <= n-x) # 右上の/|
             if c[y-1][x] == 'x' && c[y][x+1] == 'x'
               c[y][x] = '*' unless c[y][x] == 'x'
             end
-          elsif(y > n-x) # 右上の|/
+          elsif(y >= n-x) # 右上の|/
             if c[y-1][x] == 'x' && c[y][x+1] == 'x'
               c[y][x] = '*' unless c[y][x] == 'x'
             end
@@ -53,11 +79,11 @@ c.map!{|cc|
         end
       else
         if (0 <= x && x < n/2) # 左下
-          if (y < n-x) # 左下の|/
+          if (y <= n-x) # 左下の|/
             if c[y+1] && c[y+1][x] == 'x' && c[y][x-1] == 'x'
               c[y][x] = '*' unless c[y][x] == 'x'
             end
-          elsif(y > n-x) # 左下の/|
+          elsif(y >= n-x) # 左下の/|
             if c[y+1] && c[y+1][x] == 'x' && c[y][x-1] == 'x'
               c[y][x] = '*' unless c[y][x] == 'x'
             end
@@ -65,11 +91,11 @@ c.map!{|cc|
             #
           end
         else # 右下
-          if (y < x) # 右下の\|
+          if (y <= x) # 右下の\|
             if c[y+1] && c[y+1][x] == 'x' && c[y][x+1] == 'x'
               c[y][x] = '*' unless c[y][x] == 'x'
             end
-          elsif(y > x) # 右下の|/
+          elsif(y >= x) # 右下の|/
             if c[y+1] && c[y+1][x] == 'x' && c[y][x+1] == 'x'
               c[y][x] = '*' unless c[y][x] == 'x'
             end
@@ -96,6 +122,7 @@ n.times do |y|
           c[y][x] = '←'
           c[y][x] = '↑' if c[y][x-1] == 'x' || c[y][x-1] == '*'
         else # 左上の\の線
+          #
         end
       else # 右上
         if (y < n-x) # 右上の/|
@@ -135,10 +162,41 @@ n.times do |y|
 end
 
 
+n.times do |y|
+  n.times do |x|
+    if c[y][x] == ' '
+      c[y][x] = ['↑', '↓', '→', '←'].sample
+    end
+  end
+end
+
+5.times do
+  n.times do |y|
+    n.times do |x|
+      if c[y][x] == '→' && ( (c[y][x+1] ? c[y][x+1] : c[y][0]) == 'x' || (c[y][x+1] ? c[y][x+1] : c[y][0]) == '←')
+        c[y][x] = ['↑', '↓', '←'].sample
+      end
+      if c[y][x] == '←' && (c[y][x-1] == 'x' || c[y][x-1] == '→')
+        c[y][x] = ['↑', '↓', '→'].sample
+      end
+      if c[y][x] == '↑' && (c[y-1][x] == 'x' || c[y-1][x] == '↓')
+        c[y][x] = ['↓', '→', '←'].sample
+      end
+      if c[y][x] == '↓' && ((c[y+1] ? c[y+1][x] : c[0][x]) == 'x' || (c[y+1] ? c[y+1][x] : c[0][x]) == '↑')
+        c[y][x] = ['↑', '→', '←'].sample
+      end
+    end
+  end
+end
+
+
 
 
 if false
-  p [gy,gx]
+  c.map!{|cc|cc.map!{|ccc|ccc == 'x' ? ' ' : ccc}}
+  b.each do |(bx,by)|
+    c[by][bx] = 'x'
+  end
   puts '-'*n + "\n"
   puts c.map{|cc|
     cc.map{|ccc|
@@ -177,10 +235,8 @@ n.times do |i|
     when '↓' then 'D'
     else next
     end
-    if true || rand(3) != 0
-      ans << [i,ii,r]
-      k += 1
-    end
+    ans << [i,ii,r]
+    k += 1
   end
 end
 # p ans

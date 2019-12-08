72538e37d2f6002110ee3892171df892effc5c36
tetetratra <44367208+tetetratra@users.noreply.github.com>
2019-12-09 00:33:58 +0900

    ***

--- abc147/d/dd.cr
+++ abc147/d/dd.cr
@@ -1,16 +1,38 @@
-# 1 237 103715602
+# require 'pp'
+
 mod = 10 ** 9 + 7
 n = read_line.to_i
 a = read_line.split.map(&.to_i64)
-p a
-a2 = a.map{|aa|
-  tmp = Array(Int32).new(60)
+
+# p a
+ans = 0
+a2 = a.map do |aa|
+  # p aa
+  tmp = Array(Int64).new(60,0)
   60.times do |i|
-    tmp[i] = (aa % 2)
+    tmp[i] = (aa % 2).to_i64
     aa /= 2
   end
   tmp
+end
 
-}
+def kai(n, mod)
+  r = 1
+  n.times do
+    r = r * 2 % mod
+  end
+  r
+end
 
-p a2
+a2_transpose = Array( Array(Int64) ).new(60) { Array(Int64).new(n, 0) }
+# p a2_transpose
+
+n.times do |i|
+  60.times do |ii|
+    a2_transpose[ii][i] = a2[i][ii]
+  end
+end
+
+
+p a2_transpose.map{|x|x.reduce{|m,i|m+i}}.map{|x|x * (n - x)}.map_with_index{|x,i|x * kai(i,mod)}.reduce{|m,i|m+i} % mod
+# 1 237 103715602
--- abc147/d/dd.rb
+++ abc147/d/dd.rb
@@ -3,10 +3,10 @@ mod = 10 ** 9 + 7
 n = gets.to_i
 a = gets.split.map(&:to_i)
 
-p a
+# p a
 ans = 0
 a2 = a.map do |aa|
-  p aa
+  # p aa
   tmp = Array.new(60, 0)
   60.times do |i|
     tmp[i] = aa % 2
@@ -14,9 +14,7 @@ a2 = a.map do |aa|
   end
   tmp
 end
-
-p a2.transpose.map{|x|x.inject{|m,i|m + i}}.map.with_index{|x,i|x * (2**i % mod)}.inject(:+) % mod
-
-
-
+# p a2.transpose.map{|x|x.inject(:+)}.map{|x|x * (n - x)}
+# p a2.transpose.map{|x|x.inject(:+)}.map{|x|x * (n - x)}.map.with_index{|x,i|x * (2**i % mod)}#.inject(:+) % mod
+p a2.transpose.map{|x|x.inject(:+)}.map{|x|x * (n - x)}.map.with_index{|x,i|x * (2**i % mod)}.inject(:+) % mod
 # 1 237 103715602

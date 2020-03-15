46bddb75c39023d674b80f2f92940f1bd0b4bffc
tetetratra <44367208+tetetratra@users.noreply.github.com>
2020-03-15 17:34:36 +0900

    ***

--- setup.bash
+++ setup.bash
@@ -14,6 +14,13 @@ touch c/c.rb
 touch d/d.rb
 touch e/e.rb
 
-cat ~/contest/library/main.rb | tee b/b.rb | tee a/a.rb | tee c/c.rb | tee d/d.rb > e/e.rb
+echo "# a" > a/a.rb
+echo "# b" > b/b.rb
+echo "# c" > c/c.rb
+echo "# d" > d/d.rb
+echo "# e" > e/e.rb
+
+
+cat ~/Programming/contest/library/main.rb | tee -a a/a.rb | tee -a b/b.rb | tee -a c/c.rb | tee -a d/d.rb >> e/e.rb
 
 echo "SET UP OK!!"

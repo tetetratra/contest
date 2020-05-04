dc360cb22abb91dca060cbc16329f026e7b287ae
tetetratra <44367208+tetetratra@users.noreply.github.com>
2020-05-04 16:56:23 +0900

    update

---
+++ abc083/d/main.cr
@@ -0,0 +1,22 @@
+# abc083 D
+a, b, c = read_line.split.map(&.to_i)
+n = read_line.to_i
+d = read_line.split.map(&.to_i)
+s = read_line.chomp.chars
+s = read_line.chomp.chars.map(&.to_i)
+
+c = n.times.map {
+  read_line.to_i
+  read_line.split.map(&.to_i)
+  read_line.chomp.chars
+  x, y = read_line.split.map(&.to_i)
+}
+
+# ans = s.match(//)
+
+# puts (ans) ? 'Yes' : 'No'
+
+# puts (ans) ? 'YES' : 'NO'
+
+# puts c.map{|x| x.map{|xx| format("%2d",xx) }.join }.join("\n")
+
